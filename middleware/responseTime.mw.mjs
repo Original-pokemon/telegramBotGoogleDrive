953c7bd3d23d41828d10e6a92691bf9094698546
Original-pokemon <eremkin-02@mail.ru>
2023-05-21 14:19:46 +0300

    feat: add msgId in console log

--- middleware/responseTime.mw.mjs
+++ middleware/responseTime.mw.mjs
@@ -1,11 +1,19 @@
 export function responseTimeMiddleware() {
   return async (ctx, next) => {
+    const userName = ctx.session.user.Name
+    const callbackData = ctx.update.callback_query?.data
+    const messageText = ctx.update.message?.text
+    const messageId =
+      ctx.update.message?.message_id ||
+      ctx.update.callback_query?.message.message_id;
+    const messageInfo = callbackData || messageText || "mediaFile"
+    const consoleText = `${userName} :>> msgid: ${messageId}, msgInfo: ${messageInfo}`;
+    
     try {
-      const text = `${ctx.session.user.Name} :>> ${ctx.update.callback_query?.data || ctx.update.message?.text || 'photo'}`
-      console.time(text) // milliseconds
+      console.time(consoleText) // milliseconds
       // invoke downstream middleware
       await next() // make sure to `await`!
-      console.timeEnd(text)
+      console.timeEnd(consoleText)
     } catch (error) {
       console.error('Error in responseTimeMiddleware:', error)
     }
