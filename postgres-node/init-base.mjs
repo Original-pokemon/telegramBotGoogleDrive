d23edb5544d8c5bd8d5ee341302e5445c57e494e
Original-pokemon <eremkin-02@mail.ru>
2023-05-29 13:25:13 +0300

    fix: base init

--- postgres-node/init-base.mjs
+++ postgres-node/init-base.mjs
@@ -2,17 +2,16 @@ import createSchema from './set-schema.mjs';
 import createTable from './set-table.mjs';
 
 export default async function initBase(groupRepository) {
-  // try {
-  await createSchema();
-  await createTable();
-  await groupRepository.addGroup('admin');
-  await groupRepository.addGroup('waitConfirm');
-  await groupRepository.addGroup('azsWithStore');
-  await groupRepository.addGroup('azs');
-  // console.log('Database was created')
-  return true;
-  // } catch (error) {
-  //   // console.error(`initBase: ${error}`);
-  //   return false;
-  // }
+  try {
+    await createSchema();
+    await createTable();
+    await groupRepository.addGroup('admin');
+    await groupRepository.addGroup('waitConfirm');
+    await groupRepository.addGroup('azsWithStore');
+    await groupRepository.addGroup('azs');
+    return true;
+  } catch (error) {
+    console.error(`initBase: ${error}`);
+    return false;
+  }
 }
