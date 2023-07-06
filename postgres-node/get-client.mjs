32f553cf36adcb101d2dfe81be31602450974d2c
Original-pokemon <eremkin-02@mail.ru>
2023-07-06 13:00:27 +0300

    refacto: get client return Client not Pool

--- postgres-node/get-client.mjs
+++ postgres-node/get-client.mjs
@@ -1,16 +1,22 @@
+// @ts-check
 import pg from 'pg';
 
-const { Pool } = pg;
+const { Client } = pg;
 
+/**
+ * @returns {Promise<import('pg').Client>}
+ */
 export default async function getClient() {
   try {
-    const client = new Pool({
+    const client = new Client({
       host: process.env.PG_HOST,
-      port: process.env.PG_PORT,
+      port: Number(process.env.PG_PORT),
       user: process.env.PG_USER,
       password: process.env.PG_PASSWORD,
       database: process.env.PG_DATABASE,
     });
+
+    await client.connect();
 
     return client;
   } catch (error_) {
