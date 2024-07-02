bdb97b785624af8cdcfe0fb07b5c6b7f1e8dc245
tarastop12 <taras.matygin1w@gmail.com>
2024-07-02 13:30:40 +0300

    Update photoFolder.mjs

--- repositories/photoFolder.mjs
+++ repositories/photoFolder.mjs
@@ -1,19 +1,32 @@
 import { sendQuery } from '../postgres-node/sendQuery.mjs';
 
 export async function getAllFolders(client) {
-  const query = '';
-
-  await sendQuery(client, query);
+  const query = `SELECT * FROM "PhotoFolder"`;
+  const result = await sendQuery(client, query);
+  return result.rows;
 }
 
-export async function deleteFolder(client) {
-  const query = '';
-
-  await sendQuery(client, query);
+export async function deleteFolder(client,id) {
+  const query = `DELETE FROM "PhotoFolder" WHERE "Id" = '${id}';`;
+  const result = await sendQuery(client, query);
+  console.log('Successful remove:', result.rowCount);
 }
 
-export async function addFolder(client) {
-  const query = '';
+export async function addFolder(client,name,path) {
+  const query = `INSERT INTO "PhotoFolder" ("Name", "Path") VALUES ('${name}', '${path}');`;
+  const result = await sendQuery(client, query);
+}
 
-  await sendQuery(client, query);
+export async function updateFolder(client,id,name,path) {
+  const query = `UPDATE "PhotoFolder"
+  SET "Name" = '${name}', "Path" = '${path}'
+  WHERE "Id" = ${id};`;
+  const result = await sendQuery(client,query);
+  return result;
+}
+
+export async function getFolder(client, id) {
+  const query = `SELECT * FROM "PhotoFolder" WHERE "Id" = '${id}'`;
+  const result = await sendQuery(client, query);
+  return result.rows[0];
 }
