b3739ad23c1f6cb06caff743a1c949d75497eec6
original-pokemon <eremkin-02@mail.ru>
2024-08-29 15:34:51 +0300

    feat auth middleware

---
+++ src/bot/middleware/auth.mw.ts
@@ -0,0 +1,65 @@
+import { UserGroup } from '../../const.js';
+import type { Middleware } from 'grammy';
+import { Context } from '../context.js';
+
+
+
+export default function authMiddleware(): Middleware<Context> {
+  return async (ctx, next) => {
+    const { from, config, repositories: { users } } = ctx
+    const {
+      first_name: firstName,
+      last_name: lastName,
+      id,
+    } = from || {};
+
+
+
+    if (!id || !firstName) {
+      ctx.logger.warn("Authorization failed: Missing id or first name.");
+      return;
+    }
+
+    const userId = id.toString()
+
+    const firstPartUserName = [...firstName]
+      .filter((item) => item !== ' ')
+      .join('');
+    const secondPartUserName = lastName ? `_${lastName}` : '';
+    const userName = firstPartUserName + secondPartUserName;
+
+    try {
+      let user = await users.getUser(userId);
+
+      if (!user) {
+        // If the user in database
+        const group =
+          config.MAIN_ADMIN_ID === userId
+            ? UserGroup.Admin
+            : UserGroup.WaitConfirm;
+
+        user = await users.addUser({ group_id: group, id: userId.toString(), name: userName, user_folder: null });
+
+        ctx.api.sendMessage(
+          config.MAIN_ADMIN_ID,
+          `new User: ${JSON.stringify(user, undefined, '\t')}`
+        );
+      } else if (ctx.update?.my_chat_member) {
+        // If the user block bot
+        await users.deleteUser(userId);
+      }
+
+      ctx.session.user = user;
+
+      if (user.group_id === UserGroup.Admin) {
+        ctx.session.isAdmin = true
+      } else {
+        ctx.session.isAdmin = false
+      };
+
+      next();
+    } catch (error) {
+      console.error('auth.mv: \n', error);
+    }
+  };
+}
