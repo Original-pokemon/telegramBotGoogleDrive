0a345342e2c49c6930725e6ccade15dfc24af306
original-pokemon <eremkin-02@mail.ru>
2024-08-30 17:02:55 +0300

    delete postgres module

--- src/postgres-node/send-query.mjs
+++
@@ -1,8 +0,0 @@
-import getClient from './get-client.mjs';
-
-export default async function sendQuery(insertQuery) {
-  const client = await getClient();
-  const result = await client.query(insertQuery);
-  await client.end();
-  return result;
-}
