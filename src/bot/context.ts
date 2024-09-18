d396d3cc09b6632c4017516c7fc17cb9a3c6dfe5
original-pokemon <eremkin-02@mail.ru>
2024-09-18 15:33:46 +0300

    refactor: lint context

--- src/bot/context.ts
+++ src/bot/context.ts
@@ -1,35 +1,34 @@
 import { type Api, Context as DefaultContext, SessionFlavor } from "grammy";
-import { UserType } from './types/user.js';
 import type { ParseModeFlavor } from "@grammyjs/parse-mode";
-import { Logger } from '#root/logger.js';
-import { FileFlavor } from '@grammyjs/files';
-import { HydrateFlavor } from '@grammyjs/hydrate';
+import { Logger } from "#root/logger.js";
+import { FileFlavor } from "@grammyjs/files";
+import { HydrateFlavor } from "@grammyjs/hydrate";
 import { Update, UserFromGetMe } from "grammy/types";
 import { Config } from "#root/config.js";
-import QuestionRepository from "./repositories/question.repository.js";
-import UsersRepository from "./repositories/user.repository.js";
-import PhotoFolderRepository from "./repositories/photoFolder.js";
-import GroupRepository from "./repositories/group.repository.js";
 import { GoogleRepositoryType } from "#root/google-drive/index.js";
 import { Question, User } from "@prisma/client";
-import { AnswerType } from "./types/answer.js";
 import { ConversationFlavor } from "@grammyjs/conversations";
+import QuestionRepository from "./repositories/question.repository.js";
+import UsersRepository from "./repositories/user.repository.js";
+import PhotoFolderRepository from "./repositories/photo-folder.repository.js";
+import GroupRepository from "./repositories/group.repository.js";
+import { AnswerType } from "./types/answer.js";
 
 export type MemorySessionData = {
   isAdmin: boolean;
   user: User;
-}
+};
 
 export type ExternalSessionData = {
-  scene: string
+  scene: string;
   answers: (AnswerType | undefined)[];
-  questions: Question[]
+  questions: Question[];
   customData: { [key: string]: unknown };
-}
+};
 
 export type SessionData = {
-  memory: MemorySessionData
-  external: ExternalSessionData
+  memory: MemorySessionData;
+  external: ExternalSessionData;
 };
 
 export type RepositoryType = {
@@ -38,21 +37,21 @@ export type RepositoryType = {
   groups: GroupRepository;
   questions: QuestionRepository;
   photoFolders: PhotoFolderRepository;
-}
+};
 
 type ExtendedContextFlavor = {
   logger: Logger;
   repositories: RepositoryType;
-  config: Config
+  config: Config;
 };
 
 export type Context = ParseModeFlavor<
   FileFlavor<
     HydrateFlavor<
       DefaultContext &
-      ExtendedContextFlavor &
-      SessionFlavor<SessionData> &
-      ConversationFlavor
+        ExtendedContextFlavor &
+        SessionFlavor<SessionData> &
+        ConversationFlavor
     >
   >
 >;
@@ -60,34 +59,31 @@ export type Context = ParseModeFlavor<
 interface Dependencies {
   logger: Logger;
   repositories: RepositoryType;
-  config: Config
+  config: Config;
 }
 
-export function createContextConstructor(
-  {
-    logger,
-    repositories,
-    config
-  }: Dependencies,
-) {
+export function createContextConstructor({
+  logger,
+  repositories,
+  config,
+}: Dependencies) {
   return class extends DefaultContext implements ExtendedContextFlavor {
-    logger: Logger
+    logger: Logger;
 
-    repositories: RepositoryType
+    repositories: RepositoryType;
 
-    config: Config
-
+    config: Config;
 
     constructor(update: Update, api: Api, me: UserFromGetMe) {
-      super(update, api, me)
+      super(update, api, me);
 
       this.logger = logger.child({
         update_id: this.update.update_id,
-      })
+      });
 
-      this.repositories = repositories
+      this.repositories = repositories;
 
-      this.config = config
+      this.config = config;
     }
   } as unknown as new (update: Update, api: Api, me: UserFromGetMe) => Context;
-} 
+}
